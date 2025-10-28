d20cb5f70881ea8b8175b28e3803e3866295a338
NoahLacks63 <noahlacks63@gmail.com>
2025-10-28 11:02:54 -0400

    hw6 finished w/o comments

--- Lecture-0002/Homework/Homework6_NoahLacorazza/noahLacorazza_election.py
+++ Lecture-0002/Homework/Homework6_NoahLacorazza/noahLacorazza_election.py
@@ -10,8 +10,6 @@
 
 import random
 
-candidates = {}
-
 def main():
     used_numbers = set()
 
@@ -30,7 +28,7 @@ def main():
     for i in range(num_voters):
         used_nums = set()
 
-        first_choice = candidate_names[unique_int(used_nums, 0, 4)]
+        first_choice = candidate_names[rank_candidates(used_nums) - 1]
 
         candidates[first_choice].append({
             "ID" : unique_int(used_numbers, 0, 1000000),
@@ -41,8 +39,19 @@ def main():
             5 : candidate_names[rank_candidates(used_nums)]
         })
     
-    
-    
+    round = 1
+    winner = None
+    while winner == None:
+        print_round(candidates, round)
+
+        for candidate, voters in candidates.items():
+            if len(voters) > num_voters / 2:
+                winner = candidate
+        
+        round += 1
+        
+        if winner == None:
+            vote_round(candidates)
 
 def unique_int(used_numbers, min, max):
     num = random.randint(min, max)
@@ -55,21 +64,41 @@ def unique_int(used_numbers, min, max):
     return num
 
 
-def vote_round():
-    loser = list(candidates.keys()[0])
+def vote_round(candidates):
+    loser = list(candidates.keys())[0]
 
-    for key, value in candidates:
-        if candidates[loser] <= value:
+    for key, value in candidates.items():
+        if len(candidates[loser]) >= len(value):
             loser = key
     
     for voter in candidates[loser]:
-        voter.pop(voter.keys()[1])
+        voter.pop(list(voter.keys())[1])
 
-        candidates[voter.keys()[1]].append(voter)
+        next_rank = voter[list(voter.keys())[1]]
+
+        sentinal = True
+        while sentinal:
+            for candidate in candidates.keys():
+                if next_rank == candidate:
+                    sentinal = False
+            
+            if sentinal:
+                voter.pop(list(voter.keys())[1])
+                next_rank = voter[list(voter.keys())[1]]
+
+        candidates[next_rank].append(voter)
     
     candidates.pop(loser)
+    print(f"Loser: {loser}")
 
 def rank_candidates(used_nums):
     return unique_int(used_nums, 0, 4)
+
+def print_round(candidates, round):
+    print(f"\nRound: {round}")
+    print("--------------------")
+
+    for candidate, voters in candidates.items():
+        print(f"{candidate:5} | {len(voters)}")
 
 main()
